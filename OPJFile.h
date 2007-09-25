c427c0ee3c0eef2c7413278717210517c83948c9
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2007-09-25 10:45:08 +0000

    OPJFile.h version

--- OPJFile.h
+++ OPJFile.h
@@ -32,9 +32,9 @@
 #ifndef OPJFILE_H
 #define OPJFILE_H
 
-/* version 0.0 2007-08-21 */
-#define LIBORIGIN_VERSION 0x00070821
-#define LIBORIGIN_VERSION_STRING "2007-08-21"
+/* version 0.0 2007-09-25 */
+#define LIBORIGIN_VERSION 0x00070925
+#define LIBORIGIN_VERSION_STRING "2007-09-25"
 
 #include <string>
 #include <vector>
