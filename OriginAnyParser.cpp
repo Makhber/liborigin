a5c69983eb00edfb62e9ee2a0c1dfa0f928788ba
Miquel Garriga <gbmiquel@gmail.com>
2017-09-17 16:45:03 +0200

    Do not convert spreadsheet tables with only one sheet to excel tables (with more than one sheets)

--- OriginAnyParser.cpp
+++ OriginAnyParser.cpp
@@ -71,11 +71,7 @@ bool OriginAnyParser::parse() {
 	LOG_PRINT(logfile, "Now at %ld [0x%lX], filesize %d\n", curpos, curpos, d_file_size)
 
 	for(unsigned int i = 0; i < spreadSheets.size(); ++i){
-#ifdef LVERSION	// LABPLOT wants all sheets converted and not loose order
-		if(spreadSheets[i].sheets > 0){
-#else
 		if(spreadSheets[i].sheets > 1){
-#endif
 			LOG_PRINT(logfile, "		CONVERT SPREADSHEET \"%s\" to EXCEL\n", spreadSheets[i].name.c_str());
 			convertSpreadToExcel(i);
 			--i;
