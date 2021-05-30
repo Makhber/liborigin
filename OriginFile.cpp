1ff77ebb629a110aca97f51138d5a2736eb62c61
Mehdi Chinoune <mehdi.chinoune@hotmail.com>
2021-05-30 07:05:53 +0100

    Don't use std namespace in header files.

--- OriginFile.cpp
+++ OriginFile.cpp
@@ -29,8 +29,11 @@
 
 #include "config.h"
 #include "OriginFile.h"
+
 #include <fstream>
 #include <string>
+
+using namespace std;
 
 OriginFile::OriginFile(const string &fileName) : fileVersion(0), buildVersion(0), ioError(0)
 {
